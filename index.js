e752c3246c912e1bbde72502bea5bcbd5e4c657b
Dmitry <d@kozlov.ru>
2023-06-07 18:46:34 +0700

    Added documentation

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict'
-
 import jwt from 'jsonwebtoken'
 import base58 from 'bs58'
 import crypto from 'crypto'
 
-
-
-let serverSecret = base58.encode(crypto.randomBytes(32))
+// Exported functions
 
 module.exports.setServerSecret = setServerSecret
 module.exports.getSecret = getSecret
@@ -14,18 +11,32 @@ module.exports.auth = auth
 module.exports.tryAuth = tryAuth
 module.exports.parseToken = parseToken
 module.exports.parseCachedToken = parseCachedToken
-// Enable caching. This will increase performance x44000 times.
 module.exports.cacheTokens = cacheTokens
 
+// Stored values
+
+let serverSecret = base58.encode(crypto.randomBytes(32))
 let cacheSize = 0
 let cachedTokens = new Map()
 
+// Main functions
+
+/**
+ * @abstract Sets server secret. Should be called before any other function.
+ * @param {string} secret
+ */
 function setServerSecret(secret) {
   serverSecret = secret
 }
 
-// userSecret = sha512/256(serverSecret, userId).base58()
-// 208k ops/sec
+/**
+ * userSecret = sha512/256(serverSecret, userId).base58()
+ * @abstract Generates user secret from server secret and user id.
+ * @description Algorithm: userSecret = sha512/256(serverSecret, userId).base58()
+ *
+ * Performance: 208k ops
+ * @param {string} user
+ */
 function getSecret(user) {
   // sha512/256
   let sha512 = crypto.createHash('sha512').update(serverSecret).update(user).digest()
@@ -34,6 +45,13 @@ function getSecret(user) {
 }
 
 // app.auth middleware
+/**
+ * @abstract Authorization Middleware for express.js
+ * @description Will put user id to `req.from` if token is valid. Will return 401 if token is not provided. Will return 403 if token is invalid.
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
 function auth(req, res, next) {
   // Getting token from `Bearer $token`
   const token = req.headers.authorization?.split(' ').at(1)
@@ -44,6 +62,14 @@ function auth(req, res, next) {
   req.from = user
   next()
 }
+
+/**
+ * @abstract Optional Authorization Middleware for express.js
+ * @description Will put user id to `req.from` if token is valid.
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
 function tryAuth(req, res, next) {
   // Getting token from `Bearer $token`
   const token = req.headers.authorization?.split(' ').at(1)
@@ -51,11 +77,22 @@ function tryAuth(req, res, next) {
   next()
 }
 
+/**
+ * @abstract Sets cache size. Should be called before any other function. 0 - disable caching.
+ * @description Caching will increase performance x44000 times.
+ * @param {number} size
+ */
 function cacheTokens(size) {
   cacheSize = size
 }
 
 // up to 163m ops/sec
+/**
+ * @abstract Looks for token in cache. If not found, parses token and caches it.
+ * @description Performance: up to 163m ops on m1 mac
+ * @param {string} token
+ * @returns { string | undefined }
+ */
 function parseCachedToken(token) {
   if (cacheSize == 0) return parseToken(token)
   let user = cachedTokens.get(token)
@@ -67,7 +104,12 @@ function parseCachedToken(token) {
   return user
 }
 
-// 3717 ops/sec
+/**
+ * @abstract Parses token. Will return user id if token is valid. Will return undefined if token is invalid.
+ * @description Performance: 3717 ops on m1 mac
+ * @param {string} token
+ * @returns { string | undefined }
+ */
 function parseToken(token) {
   const decoded = jwt.decode(token)
   const user = decoded.user ?? decoded.sub
